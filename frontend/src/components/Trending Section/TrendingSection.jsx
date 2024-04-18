bd36ee57e59cf07403bb96709158020031ca25d0
Akarshit <akarshitsingh1234@gmail.com>
2024-04-18 22:40:30 +0530

    Added the map function in Trending Section

--- frontend/src/components/Trending Section/TrendingSection.jsx
+++ frontend/src/components/Trending Section/TrendingSection.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TrendingPost from './TrendingPost'
 import  "./trendingpost.css";
 
 const TrendingSection = () => {
+
+    const [datas , setDatas] = useState([{
+        date : "April 18 , 2024" ,
+        title : "Title 1" ,
+        description : "Description 1"
+    } , {
+        date : "April 18 , 2024" ,
+        title : "Title 2" ,
+        description : "Description 2"
+    } , {
+        date : "April 18 , 2024" ,
+        title : "Title 3" ,
+        description : "Description 3"
+    }])
     return (
         <div>
             
             <div className="App">
                 <h1 className="heading1">OUR TRENDING POST</h1>
-                <div className="posts">
-                    <TrendingPost date="April 18 , 2024" title="Title 1" description = "Description 1" />
-                    <TrendingPost date="April 18 , 2024" title="Title 2" description = "Description 2" />
-                    <TrendingPost date="April 18 , 2024" title="Title 3" description = "Description 3" />
+                <div className="posts ">
+                   {/* This Function will call the divs */}
+                   {
+                    datas.map((data)=>{
+                        return(
+                            <div><TrendingPost data={data.date} title={data.title} description={data.description}/></div>
+                        )
+                    })
+                   }
                 </div>
             </div>
             

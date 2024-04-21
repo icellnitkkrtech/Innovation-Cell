728e3db305106d0aa9dcc847b94c3ff48b8e03a3
Akarshit <akarshitsingh1234@gmail.com>
2024-04-21 21:29:06 +0530

    Again tried changing the navbar logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo() {
   return (
-    <div><img src="src/assets/IIC_Logo.png" alt="logo"  className=' w-[80px]'/></div>
+    <div><img src="\IIC_Logo.png" alt="logo"  className=' w-[80px]'/></div>
   )
 }
 

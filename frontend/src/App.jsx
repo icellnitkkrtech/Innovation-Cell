fd7e4eb2f27bce7fec41b06d2aa0f8864d1cc0d0
Akarshit <akarshitsingh1234@gmail.com>
2024-04-23 22:44:50 +0530

    Removed my Hero Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Homepage from './pages/Homepage'
 import TrendingPost from './components/Trending Section/TrendingPost';
-import Hero from './components/Hero'
+
 // import Hero from './components/Hero/Hero'
 
 function App() {

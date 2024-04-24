a7cc9275d3c779e5722456d5316249ddc2905aa1
Deependra Varshney <deependra03.jls@gmail.com>
2024-04-24 23:27:36 +0530

    ContactSection

--- frontend/src/components/ContactSection.jsx
+++ frontend/src/components/ContactSection.jsx
@@ -37,7 +37,7 @@ function ContactSection() {
       <div className="container px-5 py-24 mx-auto flex">
         <form className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
           <h2 className="text-gray-900 text-lg mb-1 font-medium title-font capitalize">
-            Contact Us
+            CONTACT US
           </h2>
           <label htmlFor="user_name" className="leading-7 text-sm text-gray-600">
             Name

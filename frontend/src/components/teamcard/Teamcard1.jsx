9b30bcbff0b82883c11984ec7973dde39ad95b90
Ayush-8955 <ayush058955@gmail.com>
2024-06-14 13:39:33 +0530

    further changes in teamcard

--- frontend/src/components/teamcard/Teamcard1.jsx
+++ frontend/src/components/teamcard/Teamcard1.jsx
@@ -127,72 +127,72 @@ const data = [
     post: 'Ideation Team Head',
     desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
   },
-  {
-    name: `Anubhav Goel`,
-    img:  `Anubhav Goel _ Ideation Team Co-Head.jpg`,
-    post: 'Ideation Team Co-Head',
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
-  },
+  // {
+  //   name: `Anubhav Goel`,
+  //   img:  `Anubhav Goel _ Ideation Team Co-Head.jpg`,
+  //   post: 'Ideation Team Co-Head',
+  //   desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
+  // },
   {
     name: `Divyanshu Yadav`,
     img:  `Divyanshu Yadav _ Finance Team Head.jpg`,
     post: 'Finance Team Head',
     desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
   },
-  {
-    name: `Dhruv Dhankhar`,
-    img:  `Dhruv Dhankhar _ Finance Team Co-Head.jpg`,
-    post: 'Finance Team Co-Head',
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
-  },
+  // {
+  //   name: `Dhruv Dhankhar`,
+  //   img:  `Dhruv Dhankhar _ Finance Team Co-Head.jpg`,
+  //   post: 'Finance Team Co-Head',
+  //   desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
+  // },
   {
     name: `Dhriti`,
     img:  `Dhriti _ Outreach Team Head.jpg`,
     post: 'Outreach Team Head',
     desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
   },
-  {
-    name: `Shobhit Sharma`,
-    img:  `Shobhit Sharma _ Outreach Team Co-Head.jpg`,
-    post: 'Outreach Team Co-Head',
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
-  },
+  // {
+  //   name: `Shobhit Sharma`,
+  //   img:  `Shobhit Sharma _ Outreach Team Co-Head.jpg`,
+  //   post: 'Outreach Team Co-Head',
+  //   desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
+  // },
   {
     name: `Akarshit`,
     img:  `Akarshit _ Tech Team Head.jpg`,
     post: 'Tech Team Head',
     desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
   },
-  {
-    name: `Dev varshaney`,
-    img:  `Dev varshaney _ Tech Team Co-Head.jpg`,
-    post: 'Tech Team Co-Head',
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
-  },
+  // {
+  //   name: `Dev varshaney`,
+  //   img:  `Dev varshaney _ Tech Team Co-Head.jpg`,
+  //   post: 'Tech Team Co-Head',
+  //   desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
+  // },
   {
     name: `Sarthak`,
     img:  `Sarthak _ Management Head.jpg`,
     post: 'Management Head',
     desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
   },
-  {
-    name: `Deepanshu Bhardwaj`,
-    img:  `Deepanshu Bhardwaj _ Management Co-Head.jpg`,
-    post: 'Management Co-Head',
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
-  },
+  // {
+  //   name: `Deepanshu Bhardwaj`,
+  //   img:  `Deepanshu Bhardwaj _ Management Co-Head.jpg`,
+  //   post: 'Management Co-Head',
+  //   desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
+  // },
   {
     name: `Vivek`,
     img:  `Vivek _ D&C Team Head.jpg`,
     post: 'D&C Team Head',
     desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
   },
-  {
-    name: `Vishesh`,
-    img:  `Vishesh _ D&C Team Co-Head.jpg`,
-    post: 'D&C Team Co-Head',
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
-  },
+  // {
+  //   name: `Vishesh`,
+  //   img:  `Vishesh _ D&C Team Co-Head.jpg`,
+  //   post: 'D&C Team Co-Head',
+  //   desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, s`
+  // },
 
 ];
 

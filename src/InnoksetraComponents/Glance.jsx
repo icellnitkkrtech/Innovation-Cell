4fb7c18290da8cba4977601da89639d4303e80d7
Deependra Varshney <deependra03.jls@gmail.com>
2024-07-19 11:33:25 +0530

    Innokshetra directory Added

---
+++ src/InnoksetraComponents/Glance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Glance() {
+  return (
+    <div>Glance</div>
+  )
+}
+
+export default Glance
---
+++ src/InnoksetraComponents/Speaker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Speaker() {
+  return (
+    <div>Speaker</div>
+  )
+}
+
+export default Speaker
